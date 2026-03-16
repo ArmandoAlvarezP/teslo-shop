4e02c010cea4bbcfe88a798491703496d5328ffd
Armando AP <armando.alvarez.gkm@hotmail.com>
2026-03-16 14:44:52 -0600

    Se agrega paginación dinámica

---
+++ src/utils/generatePaginationNumbers.ts
@@ -0,0 +1,30 @@
+
+// [1,2,3,4,5,6,7,'...', 50]
+export const generatePaginationNumbers = ( currentPage: number, totalPages: number) => {
+
+    // Si el número total de páginas es de 7 o menos se mostrarán todas las páginas sin puntos suspensivos
+    if( totalPages <= 7 ) {
+        return Array.from({ length: totalPages }, (_, i) => i + 1); // [1,2,3,4,5,6,7]
+    }
+
+    // Si la página actual está entre las primeras 3 páginas , mostrar las primeras 3, puntos suspensivos y las ultimas 2 
+    if( currentPage <= 3 ){
+        return [ 1,2,3,'...', totalPages -1, totalPages ]; // [1,2,3,'...', 49,50]
+    }
+
+    // Si la página actual está entre las últimas 3 páginas, mostrar las primeras 2, puntos suspensivos y las últimas 3 
+    if( currentPage >= totalPages - 2 ){
+        return [1,2,'...', totalPages -2, totalPages-1, totalPages ]; // [1,2,'...', 48,49,50]
+    }
+
+    // Si la página actual está en otro lugar medio , mostrar la primera página, puntos suspensivos, la página actual y vecinos 
+    return [
+        1,
+        '...',
+        currentPage -1 ,
+        currentPage,
+        currentPage +1,
+        '...',
+        totalPages
+    ];
+}
