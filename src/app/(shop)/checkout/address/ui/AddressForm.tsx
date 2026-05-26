b2e6ffe509c892fba60ecb68d31dffafb6c062fc
Armando <armando.alvarez.gkm@gmail.com>
2026-05-25 18:12:10 -0600

    Se rellena selector de paises desde bd

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import type { Country } from "@/interfaces";
 import clsx from "clsx";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
 
 type FormInputs = {
@@ -16,8 +16,11 @@ type FormInputs = {
     rememberAddress: boolean;
 }
 
-export const AddressForm = () => {
+interface Props {
+    countries: Country[];
+}
 
+export const AddressForm = ({countries}: Props ) => {
 
     const { handleSubmit, register, formState: { isValid } } = useForm<FormInputs>({
         defaultValues: {
@@ -94,7 +97,11 @@ export const AddressForm = () => {
                     { ...register('country', { required: true})}
                 >
                     <option value="">[ Seleccione ]</option>
-                    <option value="CRI">Costa Rica</option>
+                    {
+                        countries.map( country => (
+                            <option key={ country.id } value={ country.id }> { country.name } </option>
+                        ) )
+                    }
                 </select>
             </div>
 
